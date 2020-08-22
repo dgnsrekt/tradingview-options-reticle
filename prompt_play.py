d329f744aefd481f84c693c9bec4f2ae747cc002
dgnsrekt <dgnsrekt@pm.me>
2020-08-22 18:08:39 -0500

    added bullet and messed around.

---
+++ prompt_play.py
@@ -0,0 +1,18 @@
+from bullet import ScrollBar
+from bullet import emojis
+from bullet import colors
+
+cli = ScrollBar(
+    "How are you feeling today? ",
+    emojis.feelings[0],
+    height=5,
+    align=5,
+    margin=0,
+    pointer="👉",
+    background_on_switch=colors.background["default"],
+    word_on_switch=colors.foreground["default"],
+    return_index=True,
+)
+print("\n")
+result = cli.launch()
+print(result)
